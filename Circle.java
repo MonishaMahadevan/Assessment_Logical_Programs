d9b629e56698b65bf7bbc0d1676f6923abe57e13
stuvels@123 <monishamahadevan9828@gmail.com>
2023-09-15 18:24:50 +0530

    Area,Perimeter of the circle

--- Circle.java
+++ Circle.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 class Circle
 {
-    public static double Area(double radius)
+    public double Area(double radius)
     {
         double area=3.14*radius*radius;
         return area;
     }
-    public static double Perimeter(double radius)
+    public double Perimeter(double radius)
     {
         double perimeter=2*3.14*radius;
         return perimeter;
     }
     public static void main(String[]args)
     {
+        Circle a1=new Circle();
         Scanner scn=new Scanner(System.in);
         System.out.println("Enter the Radius:");
         double radius=scn.nextInt();
-        double area=Area(radius);
-        double perimeter=Perimeter(radius);
+        double area=a1.Area(radius);
+        double perimeter=a1.Perimeter(radius);
         System.out.println("The area of circle is "+area);
         System.out.println("The perimeter of circle is "+perimeter);
         
